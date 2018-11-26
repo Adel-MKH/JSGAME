6ca33733037204f0394ec1597fa9e8591dd44962
Adel Mkhenini <adel.mkhenini@gmail.com>
2018-11-26 16:48:13 +0100

    second commit

--- script/script.js
+++ script/script.js
@@ -105,5 +105,4 @@ document.body.addEventListener("keydown", function(event) {
 let keyboard = document.createElement("input");
 screen2.appendChild(keyboard);
 keyboard.setAttribute("type", "text");
-keyboard.setAttribute("type", "text");
 keyboard.style.display="none";
